65d546e8d4424148de1c76428bca00b62a2ac336
RobertoRodriguezNunez <roberto.s252272@cesurformacion.com>
2023-02-10 12:59:55 +0100

    Filter por name de composer y update de music/all

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { musiclist } from "../controllers/indexRoutes.js"
+import { musiclist, musiccomposer } from "../controllers/indexRoutes.js"
 
 const router=Router();
 
-router.get('/',musiclist);
+router.get('/api/music/all',musiclist);
+router.get('/api/music/composer/:name',musiccomposer);
 
 //ANTIGUAMENTE= module.exports = router
 export default router
